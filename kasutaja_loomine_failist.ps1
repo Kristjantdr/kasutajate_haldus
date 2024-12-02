efe2d2802062d44366dc35a7acf958b564536269
Kristjan Teder <kristjan.teder@voco.ee>
2024-12-02 10:28:41 +0200

    kasutaja loomine failiga

---
+++ kasutaja_loomine_failist.ps1
@@ -0,0 +1,10 @@
+﻿$Fail = "C:\Users\Kristjan\kasutajate_haldus\kasutajad.csv"
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+foreach ($kasutaja in $kasutajad)
+{
+$Kasutajanimi = $kasutaja.Kasutajanimi
+$Taisnimi = $kasutaja.Taisnimi
+$KontoKirjeldus = $kasutaja.KontoKirjeldus
+$Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+New-LocalUser -Name $Kasutajanimi -Password $Parool -FullName "$Taisnimi" -Description "$KontoKirjeldus"
+}
