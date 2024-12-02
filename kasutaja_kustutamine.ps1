117fa21c1693463a5b9ff38e043217f8672cab37
Kristjan Teder <kristjan.teder@voco.ee>
2024-12-02 11:11:25 +0200

    automaatne kasutaja kustutamine

---
+++ kasutaja_kustutamine.ps1
@@ -0,0 +1,19 @@
+﻿$firstName = Read-Host "Sisesta oma eesnimi"
+$lastName = Read-Host "Sisesta oma perenimi"
+
+
+$username = $firstname.ToLower() + "." + $lastname.ToLower()
+
+Write-Host 'Kustutatav kasutaja on:' $username
+
+
+try {
+    
+    Remove-LocalUser -Name $username -ErrorAction Stop
+    
+    
+    Write-Host "Kasutaja $username on edukalt kustutatud."
+} catch {
+    
+    Write-Host "Tekkinud probleem $username kustutamisega. Veateade: $_"
+}
