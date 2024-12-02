599a0dd9eacbde2fa61d56cf5dabb37aa345a0fa
Kristjan Teder <kristjan.teder@voco.ee>
2024-12-02 10:29:56 +0200

    kasutaja loomine parameetritega

---
+++ kasutaja_loomine2.ps1
@@ -0,0 +1,11 @@
+﻿if($args.Count -ne 3 ){
+echo '.\Skripti nimi kasutajanimi "Ees Perenimi" "Konto kirjeldus"'
+} else {
+
+$Kasutajanimi = $args[0]
+$Taisnimi = $args[1]
+$Kontokirjeldus = $args[2]
+)
+$KasutajaParool - ConvertTo-SecureString "qwerty" -AsPlainText -Force
+New-LocalUser "$Kasutajanimi" -Password $KasutajaParool -FullName "$Taisnimi" -Description "$KontoKirjeldus"
+}
