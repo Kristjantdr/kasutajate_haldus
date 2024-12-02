e5347b723e7f219e4e46acab4ab24f613dd611c2
Kristjan Teder <kristjan.teder@voco.ee>
2024-12-02 10:43:12 +0200

    automaatne kasutaja loomine

---
+++ kasutaja_loomine_auto.ps1
@@ -0,0 +1,17 @@
+﻿$firstname = Read-Host 'Enter your firstname'
+$lastname = Read-Host 'Enter your lastname'
+
+$username = $firstname.ToLower() + "." + $lastname.ToLower()
+$userPassword = ConvertTo-SecureString 'qwerty' -AsPlainText -Force
+$fullname = $firstname + " " + $lastname
+$description = "Local account"
+
+Write-Host 'Try to create user ' $username
+$userExists = Get-LocalUser $username -ErrorAction SilentlyContinue
+
+if(($userExists | Select Name).Name.Count -ge 1){
+    Write-Host 'Problem to create user - user already exists'
+    Exit
+    } else {
+    New-LocalUser $username -Password $userPassword -FullName $fullname -Description $description
+    }
